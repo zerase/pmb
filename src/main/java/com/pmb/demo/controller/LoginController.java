ad7ff90917115b39f89635b6195f65bff307d9cb
salome <salome@192.168.1.49>
2022-04-11 20:28:40 +0200

    Improve UI and logic in Login and Logout features

--- src/main/java/com/pmb/demo/controller/LoginController.java
+++ src/main/java/com/pmb/demo/controller/LoginController.java
@@ -1,14 +1,32 @@
 package com.pmb.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
 	@GetMapping("/login")
-	public String showLoginPage(Model model) {
+	public String showLoginView(Model model, 
+								@RequestParam(required = false) String error, 
+								@RequestParam(required = false) String logout) {
+		if(error != null) {
+			logger.error("The user failed to login");
+			model.addAttribute("error", "msg-error");
+		}
+		
+		if(logout != null) {
+			logger.info("The user logged out");
+			model.addAttribute("message", "msg-logout");
+		}
+		
+		logger.info("The user is invited to login with his credentials");
 		return "login";
 	}
 
--- src/test/java/com/pmb/demo/controller/LoginControllerTest.java
+++ src/test/java/com/pmb/demo/controller/LoginControllerTest.java
@@ -1,17 +1,22 @@
 package com.pmb.demo.controller;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -20,13 +25,39 @@ class LoginControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
 	
+	@Autowired
+	private WebApplicationContext context;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+	}
 	
 	@Test
-	@DisplayName("Test display Login page")
+	@DisplayName("Display Login page")
 	void testShowLoginView() throws Exception {
 		mockMvc.perform(get("/login")).andDo(print())
+			.andExpect(view().name("login"))
 			.andExpect(status().isOk())
 			.andExpect(content().string(containsString("login")));
+	}
+	
+	@Test
+	@DisplayName("Display Login page when user failed to login")
+	void testShowLoginView_whenUserFailedToLogIn() throws Exception {
+		mockMvc.perform(get("/login").param("error", "error")).andDo(print())
+			.andExpect(view().name("login"))
+			.andExpect(status().isOk())
+			.andExpect(content().string(containsString("Bad credentials")));
+	}
+	
+	@Test
+	@DisplayName("Display Login page when user logged out")
+	void testShowLoginView_whenUserLoggedOut() throws Exception {
+		mockMvc.perform(get("/login").param("logout", "logout")).andDo(print())
+			.andExpect(view().name("login"))
+			.andExpect(status().isOk())
+			.andExpect(content().string(containsString("You've been logged out")));
 	}
 
 }
