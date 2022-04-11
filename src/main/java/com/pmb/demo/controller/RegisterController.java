aab5950b760f77b6925b1b60b4d9345020cdb2e2
salome <salome@192.168.1.49>
2022-04-11 20:35:39 +0200

    Improve UI and logic in Register feature

--- src/main/java/com/pmb/demo/controller/RegisterController.java
+++ src/main/java/com/pmb/demo/controller/RegisterController.java
@@ -1,7 +1,10 @@
 package com.pmb.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,33 +14,56 @@ import com.pmb.demo.service.UserAccountService;
 @Controller
 public class RegisterController {
 
+	private static final Logger logger = LoggerFactory.getLogger(RegisterController.class);
+	
 	@Autowired
 	UserAccountService userAccountService;
 	
+	
 	@GetMapping("/register")
-	public String showRegisterView() {
+    public String showRegisterView(Model model, 
+                                   @RequestParam(required = false) String error,
+                                   @RequestParam(required = false) String message) {
+
+		model.addAttribute("error", error);
+		model.addAttribute("message", message);
+
+		logger.info("The user is invited to register");
 		return "register";
 	}
 	
 	// TODO: Ajouter Javadoc comment
 	@PostMapping("/register")
-	public String postRegister(@RequestParam String firstName,
-								@RequestParam String lastName,
-								@RequestParam String email,
-								@RequestParam String password) {
+	public String postRegister(Model model,
+							   @RequestParam String firstNameForm,
+							   @RequestParam String lastNameForm,
+							   @RequestParam String emailForm,
+							   @RequestParam String passwordForm) {
 		
-		String errorMail = null;
-		boolean emailAlreadyExists = userAccountService.existsUserByEmail(email);
+		String error = null;
+		String message = null;
 		
-		if(emailAlreadyExists) {
-			errorMail = "The email already exists";
+		try {
+			userAccountService.addNewUser(firstNameForm, lastNameForm, emailForm, passwordForm);
+			
+			message = "msg-register-success";
+			
+			model.addAttribute("message", message);
+			
+			logger.info("The user is registered and is invited to back to login");
+			
+		} catch (Exception e) {
+			error = "Failed to register : " + e.getMessage();
+			
+			model.addAttribute("error", error);
+			model.addAttribute("firstName", firstNameForm);
+			model.addAttribute("lastName", lastNameForm);
+			model.addAttribute("email", emailForm);
+			model.addAttribute("password", passwordForm);
+
+			logger.error("An error occur : " + e.getMessage());
 		}
-		
-		if(errorMail == null) {
-			userAccountService.addNewUser(firstName, lastName, email, password);
-			return "redirect:/register?successRegister";
-		} else {
-			return "redirect:/register?errorRegister";
-		}
+		return "register";
+
 	}
 }
--- src/test/java/com/pmb/demo/controller/RegisterControllerTest.java
+++ src/test/java/com/pmb/demo/controller/RegisterControllerTest.java
@@ -1,11 +1,14 @@
 package com.pmb.demo.controller;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 //import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -17,6 +20,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -24,6 +29,9 @@ class RegisterControllerTest {
 
 	@Autowired
 	private MockMvc mockMvc;
+	
+	@Autowired
+	private WebApplicationContext context;
 	
 	
 	@BeforeAll
@@ -36,6 +44,7 @@ class RegisterControllerTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
+		mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
 	}
 
 	@AfterEach
@@ -43,11 +52,35 @@ class RegisterControllerTest {
 	}
 
 	@Test
-	@DisplayName("Test display Register page")
+	@DisplayName("Display Register page")
 	void testShowRegisterView() throws Exception {
 		mockMvc.perform(get("/register")).andDo(print())
+			.andExpect(view().name("register"))
 			.andExpect(status().isOk())
 			.andExpect(content().string(containsString("register")));
 	}
 
+	@Test
+	@DisplayName("Display Register page when user registered successfully")
+	void testShowRegisterView_whenUserSucceedToRegister() throws Exception {
+		mockMvc.perform(post("/register").param("firstNameForm", "Marie")
+										 .param("lastNameForm", "Curie")
+										 .param("emailForm", "marie@test.com")
+										 .param("passwordForm", "test")).andDo(print())
+			.andExpect(view().name("register"))
+			.andExpect(status().isOk())
+			.andExpect(content().string(containsString("You have been registered successfully")));
+	}
+	
+	@Test
+	@DisplayName("Display Register page when user failed to register")
+	void testShowRegisterView_whenUserFailedToRegister() throws Exception {
+		mockMvc.perform(post("/register").param("firstNameForm", "John")
+				 						 .param("lastNameForm", "Ford")
+				 						 .param("emailForm", "john@test.com")
+				 						 .param("passwordForm", "test")).andDo(print())
+			.andExpect(view().name("register"))
+			.andExpect(status().isOk())
+			.andExpect(content().string(containsString("Failed to register")));
+	}
 }
