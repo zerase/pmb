4b7051c48963cb6c8538c9154ad6d00c0039d44b
salome <salome@192.168.1.49>
2022-04-18 15:56:38 +0200

    Improve UI in Contact feature

--- src/main/java/com/pmb/demo/controller/ContactController.java
+++ src/main/java/com/pmb/demo/controller/ContactController.java
@@ -1,7 +1,10 @@
 package com.pmb.demo.controller;
 
 import java.util.Iterator;
+import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,46 +12,50 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.pmb.demo.model.BankAccount;
 import com.pmb.demo.model.UserAccount;
 import com.pmb.demo.service.UserAccountService;
 
 @Controller
 public class ContactController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(ContactController.class);
+	
 	@Autowired
 	private UserAccountService userAccountService;
-	
-	
-	// XXX: Retourne la page contacts contenant la liste d'un user
+
+
 	@GetMapping("/contact")
 	public String showContactView(Model model) {
 
 		// Get current authenticated user
 		UserAccount user = userAccountService.getCurrentConnectedUser().orElseThrow();
+		Optional<BankAccount> userBank = Optional.ofNullable(user.getBankAccount());
 		
 		Iterator<UserAccount> userfriends = userAccountService.getUserConnectionsByMail(user.getEmail());
 
 		// Return user information to view
 		model.addAttribute("username", user.getFirstName() + " " +user.getLastName());
 		model.addAttribute("userbalance", user.getBalance());
+		model.addAttribute("userbank", userBank);
 		model.addAttribute("userconnections", userfriends);
-		
+		logger.info("The user is invited to add a connection to his list");
 		return "contact" ;
 	}
-	
-	
-	// XXX: Formulaire d'ajout d'un nouveau contact à sa liste
+
+
 	@PostMapping("/contact")
-	public String addNewConnection(Model model, @RequestParam String email) {
+	public String addNewConnection(@RequestParam String email, Model model) {
 
 		// Get current authenticated user
 		UserAccount user = userAccountService.getCurrentConnectedUser().orElseThrow();
 
 		try {
 			userAccountService.addConnection(user.getEmail(), email);
+			logger.info("The user add a connection successfully");
 			return "redirect:/contact?successAddConnection";
 		} catch (Exception e) {
-			System.out.println(e);
+			logger.error("An error occur : " + e.getMessage());
 			return "redirect:/contact?errorAddConnection";
 		}
 	}
